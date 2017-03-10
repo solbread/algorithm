0849d3dd89664cbf8d1ad09940d2187e563f1b2a
JungSol <solbread0@gmail.com>
2017-03-10 21:34:33 +0900

    해결

---
+++ src/com/algorithm/container_with_most_water/BestSolution.java
@@ -0,0 +1,20 @@
+package com.algorithm.container_with_most_water;
+
+public class BestSolution {
+	//Time complexity : O(n)
+	public int maxArea(int[] height) {
+		int p = 0, q = height.length - 1, maxArea = Integer.MIN_VALUE;
+		while (p < q) {
+			maxArea = Math.max(maxArea, (q - p) * Math.min(height[p], height[q]));
+			if (height[p] < height[q]) p++;
+			else q--;
+		}
+		return maxArea;
+	}
+	
+    public static void main(String[] args) {
+		BestSolution solution = new BestSolution();
+		int height[] = {3, 4, 4, 3};
+		System.out.println(solution.maxArea(height));
+	}
+}
