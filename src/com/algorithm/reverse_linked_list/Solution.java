bd9b29235caf67225477615ec83cd1c287985e0f
JungSol <solbread0@gmail.com>
2017-03-23 22:53:57 +0900

    답안2(통과)
    전역에 result라는 ListNode를 두고 여기에 누적을 하도록 개선

--- src/com/algorithm/reverse_linked_list/Solution.java
+++ src/com/algorithm/reverse_linked_list/Solution.java
@@ -3,24 +3,27 @@ package com.algorithm.reverse_linked_list;
 import com.algorithm.etc.ListNode;
 
 public class Solution {
-    public ListNode reverseList(ListNode head) {
-        if(head==null) return null;
-		return reverse(head);
+	ListNode result;
+	public ListNode reverseList(ListNode head) {
+		if(head==null) return null;
+		reverse(head);
+		return result;
 	}
 	private ListNode reverse(ListNode head) {
 		if(head.next == null) {
-			return new ListNode(head.val);
+			result = new ListNode(head.val);
+			return result;
 		}
 		ListNode tmp = reverse(head.next);
 		tmp.next = new ListNode(head.val);
-		return tmp;
+		return tmp.next;
 	}
 	public static void main(String[] args) {
 		Solution solution = new Solution();
 		ListNode head = new ListNode(2);
 		head.next = new ListNode(3);
 		head.next.next = new ListNode(1);
-		ListNode reversedNode = solution.reverse(head);
+		ListNode reversedNode = solution.reverseList(head);
 		while(reversedNode!=null) {
 			System.out.println(reversedNode.val);
 			reversedNode = reversedNode.next;
