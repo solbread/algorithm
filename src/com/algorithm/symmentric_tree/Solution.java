0d7f2942f631bc1002999116bed031c1e7d33c51
JungSol <solbread0@gmail.com>
2017-03-24 22:22:56 +0900

    답안

---
+++ src/com/algorithm/symmentric_tree/Solution.java
@@ -0,0 +1,22 @@
+package com.algorithm.symmentric_tree;
+
+import com.algorithm.etc.TreeNode;
+
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+    	if(root==null) return true;
+    	return check(root.left, root.right);
+    }
+    private boolean check(TreeNode node1, TreeNode node2) {
+    	if(node1 == null || node2 == null) return node1==node2;
+    	if(node1.val != node2.val) return false;
+    	return check(node1.left, node2.right) && check(node1.right, node2.left);
+    }
+    public static void main(String[] args) {
+    	TreeNode root = new TreeNode(2);
+    	root.left = new TreeNode(4);
+    	root.right = new TreeNode(2);
+		Solution solution = new Solution();
+		System.out.println(solution.isSymmetric(root));
+	}
+}
