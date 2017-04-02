4063bccd94fc063ba0f668b8e254cd92605b936a
JungSol <solbread0@gmail.com>
2017-04-03 00:10:58 +0900

    1차답안(wrong answer)

---
+++ src/com/algorithm/trie/maximum_xor_of_two_numbers_in_an_array/Solution.java
@@ -0,0 +1,38 @@
+package com.algorithm.trie.maximum_xor_of_two_numbers_in_an_array;
+
+public class Solution {
+	public int findMaximumXOR(int[] nums) {
+		Object[] root = {null, null};
+		for(int num : nums) {
+			Object[] current = root;
+			for(int i = 0; i < 32; i ++) {
+				int bit = (num>>i)&1;
+				if(current[bit]==null) {
+					current[bit] = new Object[]{null, null};
+				}
+				current = (Object[]) current[bit];
+			}
+		}
+		int maximumXor = 0;
+		for(int num : nums) {
+			int currentMaximumXor = 0;
+			Object[] current = root;
+			for(int i = 0; i < 32; i ++) {
+				int bit = (num>>i)&1;
+				if(current[bit^1]==null) {
+					current = (Object[]) current[bit];
+				} else {
+					currentMaximumXor += (1<<i);
+					current = (Object[]) current[bit^1];
+				}
+			}
+			maximumXor = Math.max(maximumXor,currentMaximumXor);
+		}
+		return maximumXor;
+	}
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {3, 10, 5, 25, 2, 8};
+        System.out.println(solution.findMaximumXOR(nums)); //28
+    }
+}
