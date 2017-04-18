a233c55046cc40de78a375f2be936cf32f047a6b
JungSol <solbread0@gmail.com>
2017-04-18 22:50:52 +0900

    refactoring - simplify regex

--- src/com/algorithm/training/string/student_attendance_record1/Solution.java
+++ src/com/algorithm/training/string/student_attendance_record1/Solution.java
@@ -2,7 +2,7 @@ package com.algorithm.training.string.student_attendance_record1;
 
 public class Solution {
     public boolean checkRecord(String s) {
-        return !(s.matches(".*A.*A.*") || s.matches(".*LLL.*"));
+        return !s.matches(".*A.*A.*|.*LLL.*");
     }
     public static void main(String[] args) {
     	Solution solution = new Solution();
