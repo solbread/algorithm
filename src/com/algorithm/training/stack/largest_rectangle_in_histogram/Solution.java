b387a3f3ece2fb67e13e191b72a1db8f97ccf668
JungSol <solbread0@gmail.com>
2017-04-18 23:38:26 +0900

    try2 (wrong anwer)
    너비를 구할 때 start, end 범위에 있는 최소 높이를 이용하도록 수정하였으나 여전히 wrong answer (
    [5,5,1,7,1,1,5,2,7,6]일 때 기대output은 12지만 나의 답은 10)

--- src/com/algorithm/training/stack/largest_rectangle_in_histogram/Solution.java
+++ src/com/algorithm/training/stack/largest_rectangle_in_histogram/Solution.java
@@ -1,17 +1,27 @@
 package com.algorithm.training.stack.largest_rectangle_in_histogram;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class Solution {
     public int largestRectangleArea(int[] height) {
         if(height==null || height.length==0) return 0;
         int largestArea = height[0], start = 0, end = height.length-1;
+        
+        Queue<Integer> queue = new PriorityQueue<>();
+        for(int heightVal : height) {
+        	queue.add(heightVal);
+        }
         while(start!=end) {
             if(height[start] > height[end]) {
                 largestArea = Math.max(largestArea, height[start]);
-                largestArea = Math.max(largestArea, height[end]*(end-start+1));
+                largestArea = Math.max(largestArea, queue.peek()*(end-start+1));
+                queue.remove(height[end]);
                 end--;
             } else {
                 largestArea = Math.max(largestArea, height[end]);
-                largestArea = Math.max(largestArea, height[start]*(end-start+1));
+                largestArea = Math.max(largestArea, queue.peek()*(end-start+1));
+                queue.remove(height[start]);
                 start++;
             }
         }
@@ -20,7 +30,8 @@ public class Solution {
     
     public static void main(String[] args) {
 		Solution solution = new Solution();
-		int height[] = {2, 1, 5, 6, 2, 3};
-		System.out.println(solution.largestRectangleArea(height)); //output:10
+//		int height[] = {2, 1, 5, 6, 2, 3}; //output:10
+		int height[] = {5,5,1,7,1,1,5,2,7,6}; //output:12
+		System.out.println(solution.largestRectangleArea(height));
 	}
 }
