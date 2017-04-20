b278a363fe9b80ff33a00b55854904236e47989a
JungSol <solbread0@gmail.com>
2017-04-20 23:52:14 +0900

    try1 wrong answer
    input : 2  output : 2/ expected : 2

---
+++ src/com/algorithm/training/math/README.md
@@ -0,0 +1,7 @@
+## Algorithm Problem related on Math
+
+**Problem list**
+* Easy
+* Medium
+	* Optimal Division (\#string)
+* Hard
---
+++ src/com/algorithm/training/math/optimal_division/BestSolution.java
@@ -0,0 +1,22 @@
+package com.algorithm.training.math.optimal_division;
+
+public class BestSolution {
+    public String optimalDivision(int[] nums) {
+        StringBuilder result = new StringBuilder();
+        for(int i = 0; i < nums.length; i++) {
+        	if(i==1 && nums.length > 2) {
+        		result.append('(');
+        	}
+        	result.append(nums[i]).append('/');
+        	if(i == nums.length-1 && nums.length > 2) {
+        		result.setCharAt(result.length()-1, ')');
+        	}
+        }
+    	return result.toString();
+    }
+    public static void main(String[] args) {
+		BestSolution solution = new BestSolution();
+		int nums[] = {1000, 100, 10, 2}; //output:1000/(100/10/2)
+		System.out.println(solution.optimalDivision(nums));
+	}
+}
