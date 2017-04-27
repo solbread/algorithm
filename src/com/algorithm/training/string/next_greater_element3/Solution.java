8aa8e4593a6d65cbf161e38cd09013f0fe11162c
JungSol <solbread0@gmail.com>
2017-04-27 23:39:43 +0900

    try 6 (wrong answer)
    단어를 분리하는 타이밍을 뒤에서부터 순회하며 맨 마지막 문자보다 작은 문자가 나왔을때가 아닌, 뒤에서 부터 순회하며 뒤의 문자보다
    작은 문자가 등장한 시점으로 변경. input : 1200000 output : 12 expected : 2000001

--- src/com/algorithm/training/string/next_greater_element3/Solution.java
+++ src/com/algorithm/training/string/next_greater_element3/Solution.java
@@ -7,13 +7,20 @@ public class Solution {
         int nextGreaterNumber = -1;
         String number = String.valueOf(n);
         for(int i = number.length()-2; i >= 0; i--) {
-        	if(number.charAt(i) < number.charAt(number.length()-1)) {
+        	if(number.charAt(i) < number.charAt(i+1)) {
         		StringBuilder sb = new StringBuilder();
         		if(i > 0) sb.append(number.substring(0, i));
-        		sb.append(number.charAt(number.length()-1));
-        		char[] remainder = number.substring(i, number.length()-1).toCharArray();
+        		char[] remainder = number.substring(i, number.length()).toCharArray();
+        		int targetIdx = 1;
+        		for(int j = 2; j < remainder.length; j++) {
+        			if(remainder[j] > number.charAt(i) && remainder[j] < remainder[targetIdx]) {
+        				targetIdx = j;
+        			}
+        		}
         		Arrays.sort(remainder);
+        		sb.append(remainder[targetIdx]);
         		for(int j = 0; j < remainder.length; j++) {
+        			if(j==targetIdx) continue;
         			sb.append(remainder[j]);
         		}
         		try {
@@ -29,6 +36,8 @@ public class Solution {
     
     public static void main(String[] args) {
 		Solution solution = new Solution();
+		System.out.println(solution.nextGreaterElement(1)); //output:-1
 		System.out.println(solution.nextGreaterElement(12222333)); //output:12223233
+		System.out.println(solution.nextGreaterElement(12443322)); //output:13222344
 	}
 }
