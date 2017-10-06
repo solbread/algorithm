2e755db42ad6247eac1c6ff8a2971118e74a4c8b
JungSol <solbread0@gmail.com>
2017-10-06 11:30:05 +0900

    답안1 (try 2, exceed time)
    기존에 판자들중에 가장 긴 판자에서 출발해서 넓이의 최대값을 찾았었는데, 이렇게 되면 무조건 특정판자(가장 긴 판자)를 포함하게 되므로, 모든 점에서 시작하도록 변경

--- src/com/algorithm/training/divide_and_conquer/fence/Main.java
+++ src/com/algorithm/training/divide_and_conquer/fence/Main.java
@@ -16,24 +16,24 @@ public class Main {
 		int cases = scanner.nextInt();
 		while(cases-- > 0) {
 			boardHeights = new int[scanner.nextInt()];
-			int maxHeight = -1, left = -1, right = -1;
 			for(int i = 0; i < boardHeights.length; i++) {
 				boardHeights[i] = scanner.nextInt();
-				if(maxHeight < boardHeights[i]) {
-					maxHeight = boardHeights[i];
-					left = right = i;
-				}
 			}
-			int maxWeight = boardHeights[left], currentMinHeight = maxWeight;
-			while(left > 0 || right < boardHeights.length-1) {
-				if((left <= 0 && right < boardHeights.length-1) || (right < boardHeights.length -1 && boardHeights[left-1] < boardHeights[right+1])) {
-					right++;
-					currentMinHeight = Math.min(currentMinHeight, boardHeights[right]);
-				} else if((left > 0 && right >= boardHeights.length-1) || (left > 0 && boardHeights[left-1] >= boardHeights[right+1])) {
-					left--;
-					currentMinHeight = Math.min(currentMinHeight, boardHeights[left]);
+			int maxWeight = -1;
+			for(int i = 0; i < boardHeights.length; i++) {
+				int left = i, right = left, currentMinHeight = boardHeights[i];
+				maxWeight = Math.max(maxWeight, boardHeights[i]);
+				while(left > 0 || right < boardHeights.length-1) {
+					if((left <= 0 && right < boardHeights.length-1) || (right < boardHeights.length -1 && boardHeights[left-1] < boardHeights[right+1])) {
+						right++;
+						currentMinHeight = Math.min(currentMinHeight, boardHeights[right]);
+					} else if((left > 0 && right >= boardHeights.length-1) || (left > 0 && boardHeights[left-1] >= boardHeights[right+1])) {
+						left--;
+						currentMinHeight = Math.min(currentMinHeight, boardHeights[left]);
+					}
+					maxWeight = Math.max(maxWeight, Math.max(Math.max(boardHeights[left], boardHeights[right]), currentMinHeight * (right - left + 1)));
 				}
-				maxWeight = Math.max(maxWeight, Math.max(Math.max(boardHeights[left], boardHeights[right]), currentMinHeight * (right - left + 1)));
+				
 			}
 			System.out.println(maxWeight);
 		}
