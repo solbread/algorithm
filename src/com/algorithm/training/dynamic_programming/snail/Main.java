fb04027c648084ac05488de6cd4a236dcfe82909
JungSol <solbread0@gmail.com>
2017-10-12 00:03:27 +0900

    답안1 (refactoring, try1) solve

--- src/com/algorithm/training/dynamic_programming/snail/Main.java
+++ src/com/algorithm/training/dynamic_programming/snail/Main.java
@@ -2,10 +2,12 @@ package com.algorithm.training.dynamic_programming.snail;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-	static int[][] cache;
+	static int wellDepth;
+	static double[][] cache;
 	public static void main(String[] args) {
 		Main main = new Main();
 		Scanner scanner = new Scanner(System.in);
@@ -16,14 +18,17 @@ public class Main {
 		}
 		int cases = scanner.nextInt();
 		while(cases-- > 0) {
-			int wellDepth = scanner.nextInt();
+			wellDepth = scanner.nextInt();
 			int monsoonDuration = scanner.nextInt();
-			cache = new int[wellDepth][monsoonDuration];
-			System.out.println(main.getEscapeProbaility(wellDepth, monsoonDuration));
+			cache = new double[2 * monsoonDuration+1][monsoonDuration];
+			for(double[] row :cache) Arrays.fill(row, -1);
+			System.out.println(main.getEscapeProbaility(0, monsoonDuration));
 		}
 	}
-	public double getEscapeProbaility(int remainDepth, int remainDuration) {
-		if(remainDuration == 0) return remainDepth <= 0 ? 1 : 0;
-		return (0.25 * getEscapeProbaility(remainDepth-1, remainDuration-1)) + (0.75 * getEscapeProbaility(remainDepth-2, remainDuration-1));
+	public double getEscapeProbaility(int currentDepth, int remainDuration) {
+		if(remainDuration == 0) return currentDepth >= wellDepth ? 1 : 0;
+		if(cache[currentDepth][remainDuration-1] != -1) return cache[currentDepth][remainDuration-1];
+		cache[currentDepth][remainDuration-1] = (0.25 * getEscapeProbaility(currentDepth+1, remainDuration-1)) + (0.75 * getEscapeProbaility(currentDepth+2, remainDuration-1));
+		return cache[currentDepth][remainDuration-1];
 	}
 }
