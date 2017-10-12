94640f387d6034e6dc913d36f28185cba09aacfc
JungSol <solbread0@gmail.com>
2017-10-12 11:06:00 +0900

    답안1(try6, refactoring)
    최소 반복횟수를 찾을 때 만들어진 repeatedA를 이용하여 찾도록 리펙토링 (코드 간결해짐)

--- src/com/algorithm/training/string/repeat_string_match/Solution.java
+++ src/com/algorithm/training/string/repeat_string_match/Solution.java
@@ -2,21 +2,17 @@ package com.algorithm.training.string.repeat_string_match;
 
 public class Solution {
     public int repeatedStringMatch(String A, String B) {
-        StringBuilder sb = new StringBuilder(A);
+        StringBuilder repeatedA = new StringBuilder(A);
         for(int i = 0; i < Math.ceil((B.length()-1)/(double)A.length()); i++) {
-            sb.append(A);
+            repeatedA.append(A);
         }
-        if(!sb.toString().contains(B)) return -1;
-        int repeatCount = 1, aIndex = 0, bIndex = 0;
+        if(!repeatedA.toString().contains(B)) return -1;
+        int aIndex = 0, bIndex = 0;
         while(bIndex < B.length()) {
-            if(aIndex == A.length()) {
-                repeatCount++;
-                aIndex = 0;
-            }
-            bIndex = (B.charAt(bIndex) == A.charAt(aIndex)) ? bIndex+1 : 0;
+            bIndex = (B.charAt(bIndex) == repeatedA.charAt(aIndex)) ? bIndex+1 : 0;
             aIndex++;
         }
-        return repeatCount;
+        return ((aIndex-1)/A.length()) + 1;
     }
     
     public static void main(String[] args) {
