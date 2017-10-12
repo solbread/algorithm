6139cfc68454e8cd39f7eb7141b1944c0fda992c
JungSol <solbread0@gmail.com>
2017-10-12 10:44:08 +0900

    답안1(try4) memory limit exceed
    A를 n번 반복해서 B를 만들 수 있는지 없는지 판단하는 부분 수정 (aIndex가 끝났을때 남은 B를 set에 넣어서 루프가 ㅂ라생하면 B를 만들 수 없다고 판단)

--- src/com/algorithm/training/string/repeat_string_match/Solution.java
+++ src/com/algorithm/training/string/repeat_string_match/Solution.java
@@ -1,23 +1,19 @@
 package com.algorithm.training.string.repeat_string_match;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
     public int repeatedStringMatch(String A, String B) {
-        boolean isMatched = false;
+        Set<String> remainedNumbers = new HashSet<>();
         int repeatCount = 1, aIndex = 0, bIndex = 0;
         while(bIndex < B.length()) {
             if(aIndex == A.length()) {
-                if(!isMatched) return -1;
+                if(!remainedNumbers.add(B.substring(bIndex))) return -1;
                 repeatCount++;
                 aIndex = 0;
-                isMatched = false;
             }
-            if(B.charAt(bIndex) == A.charAt(aIndex)) {
-                bIndex++;
-                isMatched = true;
-            } else {
-                isMatched = false;
-                bIndex = 0;
-            }
+            bIndex = (B.charAt(bIndex) == A.charAt(aIndex)) ? bIndex+1 : 0;
             aIndex++;
         }
         return repeatCount;
@@ -29,5 +25,8 @@ public class Solution {
         System.out.println(solution.repeatedStringMatch("abd", "cccc")); //-1
         System.out.println(solution.repeatedStringMatch("a", "aa")); //2
         System.out.println(solution.repeatedStringMatch("abcabcabcabc", "abac")); //-1
+        System.out.println(solution.repeatedStringMatch("abacababacab", "abcdabcdabcdabcd"));
 	}
 }
+
+
