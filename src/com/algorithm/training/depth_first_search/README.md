00359863180c87eca7eeadbb5bcb96695acd2421
JungSol <solbread0@gmail.com>
2017-10-13 14:15:13 +0900

    답안1(try1) wrong answer

---
+++ src/com/algorithm/training/depth_first_search/README.md
@@ -0,0 +1,10 @@
+## Algorithm Problem related on Depth First Search
+
+**Problem list**
+* Easy
+
+* Medium
+	* Redundant Connection
+
+* Hard
+	* Redundant Connection2
---
+++ src/com/algorithm/training/depth_first_search/redundant_connection/Solution.java
@@ -0,0 +1,31 @@
+package com.algorithm.training.depth_first_search.redundant_connection;
+
+public class Solution {
+    int[] parents;
+    public int[] findRedundantConnection(int[][] edges) {
+        parents = new int[1001];
+        for(int i = 0; i < parents.length; i++) {
+            parents[i] = i;
+        }
+        int[] result = null;
+        for(int[] edge : edges) {
+            int parent1 = find(edge[0]);
+            int parent2 = find(edge[1]);
+            if(parent1 == parent2) result = edge; 
+            parents[edge[1]] = edge[0];
+        }
+        return result;
+    }
+    
+    public int find(int point) {
+        if(parents[point] == point) return point;
+        return find(parents[point]);
+    }
+    
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] result = solution.findRedundantConnection(new int[][]{{1,2},{1,3},{2,3}});
+        result = solution.findRedundantConnection(new int[][]{{1,2},{2,3},{3,4},{1,4},{1,5}});
+        System.out.println(result[0] + ", " + result[1]);
+    }
+}
