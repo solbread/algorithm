93c4bc1c820b49b069273fea826ec5f4c769b1af
JungSol <solbread0@gmail.com>
2017-10-23 16:07:12 +0900

    답안1(try2) wrong answer

--- src/com/algorithm/training/tree/longest_univalue_path/Solution.java
+++ src/com/algorithm/training/tree/longest_univalue_path/Solution.java
@@ -4,37 +4,42 @@ import com.algorithm.training.etc.TreeNode;
 
 public class Solution {
     public int longestUnivaluePath(TreeNode root) {
+        if(root == null) return 0;
+        Solution solution = new Solution();
+        return solution.getLongestPath(root, 0);
+    }
+    private int getLongestPath(TreeNode root, int prevPathType) {
         int longestPathLength = 0;
-        if(root.left != null && root.val == root.left.val) {
-            longestPathLength += (1 + longestUnivaluePath(root.left));
+        if(root.left != null && root.val == root.left.val && (prevPathType == 0 || prevPathType == 1)) {
+            longestPathLength += (1 + getLongestPath(root.left, 1));
         }
-        if(root.right != null && root.val == root.right.val) {
-            longestPathLength += (1 + longestUnivaluePath(root.right));
+        if(root.right != null && root.val == root.right.val && (prevPathType == 0 || prevPathType == 2)) {
+            longestPathLength += (1 + getLongestPath(root.right, 2));
         }
         if(root.left != null && root.val != root.left.val) {
-            longestPathLength = Math.max(longestPathLength, longestUnivaluePath(root.left));
+            longestPathLength = Math.max(longestPathLength, getLongestPath(root.left, 0));
         }
         if(root.right != null && root.val != root.right.val) {
-            longestPathLength = Math.max(longestPathLength, longestUnivaluePath(root.right));
+            longestPathLength = Math.max(longestPathLength, getLongestPath(root.right, 0));
         }
         return longestPathLength;
     }
     public static void main(String[] args) {
         Solution solution = new Solution();
         
-//        TreeNode root = new TreeNode(5); //2
-//        root.left = new TreeNode(4);
-//        root.right = new TreeNode(5);
-//        root.left.left = new TreeNode(1);
-//        root.left.right = new TreeNode(1);
-//        root.right.right = new TreeNode(5);
-        
-        TreeNode root = new TreeNode(1); //2
+        TreeNode root = new TreeNode(5); //2
         root.left = new TreeNode(4);
         root.right = new TreeNode(5);
-        root.left.left = new TreeNode(4);
-        root.left.right = new TreeNode(4);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(1);
         root.right.right = new TreeNode(5);
+        
+//        TreeNode root = new TreeNode(1); //2
+//        root.left = new TreeNode(4);
+//        root.right = new TreeNode(5);
+//        root.left.left = new TreeNode(4);
+//        root.left.right = new TreeNode(4);
+//        root.right.right = new TreeNode(5);
         
         System.out.println(solution.longestUnivaluePath(root));
     }
