d15198dce46fc96172fabb6fe21d1ae7c5590ff9
JungSol <solbread0@gmail.com>
2017-10-26 15:35:40 +0900

    답안1(try1) wrong answer

--- src/com/algorithm/training/dynamic_programming/partition_to_k_equal_sum_subsets/Solution.java
+++ src/com/algorithm/training/dynamic_programming/partition_to_k_equal_sum_subsets/Solution.java
@@ -9,18 +9,41 @@ public class Solution {
         boolean canPartitionKSubSets = false;
         int sum = IntStream.of(nums).sum();
         this.average = sum / 4;
-        if(sum%k == 0) return canPartitionSubsets(new boolean[nums.length], -1, k);
         this.nums = nums;
+        if(sum%k == 0) return canPartitionSubsets(new boolean[nums.length], -1, this.average, k+1);
         return canPartitionKSubSets;
     }
-    public boolean canPartitionSubsets(boolean[] isVisited, int startPoint, int k) {
-        boolean canPartitionKSubSets = false;
+    public boolean canPartitionSubsets(boolean[] isVisited, int startPoint, int targetSum, int k) {
+        if(startPoint != -1) isVisited[startPoint] = true;
+        if(k==0) return isAllVisited(isVisited);
         for(int i = startPoint + 1; i < nums.length; i++) {
-            if(nums[startPoint] + nums[i] < this.average) {
-                
+            if(startPoint == -1 || nums[startPoint] + nums[i] == targetSum) {
+                isVisited[i] = true;
+                if(canPartitionSubsets(isVisited, getNextTargetIndex(isVisited), average, k-1)) return true;;
+                isVisited[i] = false;
+            } else if(nums[startPoint] + nums[i] < targetSum) {
+                if(canPartitionSubsets(isVisited, i, targetSum - nums[startPoint] - nums[i], k)) return true;
             }
         }
-        return canPartitionKSubSets;
+        if(startPoint != -1) isVisited[startPoint] = false;
+        return false;
+    }
+    
+    private boolean isAllVisited(boolean[] isVisited) {
+        for(boolean v : isVisited) {
+            if(!v) return false;
+        }
+        return true;
+    }
+    
+    private int getNextTargetIndex(boolean isVisited[]) {
+        int index = -1;
+        for(int i = 0; i < isVisited.length; i++) {
+            if(!isVisited[i]) {
+                return i;
+            }
+        }
+        return index;
     }
     public static void main(String[] args) {
     	Solution solution = new Solution();
