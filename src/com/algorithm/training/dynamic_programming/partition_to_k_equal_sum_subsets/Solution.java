dbe7a373d7d64364930c840343cee35f3f172f90
JungSol <solbread0@gmail.com>
2017-10-26 16:45:18 +0900

    답안1(try2) wrong answer
    1. average 계산할때 변수k로 나눠주도록 수정
    2. nums[startPoint]==targetSum일때 처리 추가

--- src/com/algorithm/training/dynamic_programming/partition_to_k_equal_sum_subsets/Solution.java
+++ src/com/algorithm/training/dynamic_programming/partition_to_k_equal_sum_subsets/Solution.java
@@ -8,24 +8,35 @@ public class Solution {
     public boolean canPartitionKSubsets(int[] nums, int k) {
         boolean canPartitionKSubSets = false;
         int sum = IntStream.of(nums).sum();
-        this.average = sum / 4;
+        this.average = sum / k;
         this.nums = nums;
-        if(sum%k == 0) return canPartitionSubsets(new boolean[nums.length], -1, this.average, k+1);
-        return canPartitionKSubSets;
-    }
-    public boolean canPartitionSubsets(boolean[] isVisited, int startPoint, int targetSum, int k) {
-        if(startPoint != -1) isVisited[startPoint] = true;
-        if(k==0) return isAllVisited(isVisited);
-        for(int i = startPoint + 1; i < nums.length; i++) {
-            if(startPoint == -1 || nums[startPoint] + nums[i] == targetSum) {
-                isVisited[i] = true;
-                if(canPartitionSubsets(isVisited, getNextTargetIndex(isVisited), average, k-1)) return true;;
-                isVisited[i] = false;
-            } else if(nums[startPoint] + nums[i] < targetSum) {
-                if(canPartitionSubsets(isVisited, i, targetSum - nums[startPoint] - nums[i], k)) return true;
+        
+        if(sum%k == 0) {
+            for(int i = 0; i < nums.length;i ++) {
+                if(canPartitionSubsets(new boolean[nums.length], i, this.average, k)) return true;
             }
         }
-        if(startPoint != -1) isVisited[startPoint] = false;
+        return canPartitionKSubSets;
+    }
+    
+    public boolean canPartitionSubsets(boolean[] isVisited, int startPoint, int targetSum, int k) {
+        if(startPoint != -1) isVisited[startPoint] = true;
+        if(k == 0) return isAllVisited(isVisited);
+        if(nums[startPoint] == targetSum) {
+            if(canPartitionSubsets(isVisited, getNextTargetIndex(isVisited), average, k-1)) return true;
+        }
+        for(int i = startPoint+1; i < nums.length; i++) {
+            if(!isVisited[i]) {
+                if(nums[startPoint] + nums[i] == targetSum) {
+                    isVisited[i] = true;
+                    if(canPartitionSubsets(isVisited, getNextTargetIndex(isVisited), average, k-1)) return true;
+                    isVisited[i] = false;
+                } else if(nums[startPoint] + nums[i] < targetSum) {
+                    if(canPartitionSubsets(isVisited, i, targetSum - nums[startPoint] - nums[i], k)) return true;
+                }
+            }
+        }
+        if(startPoint != -1)isVisited[startPoint] = false;
         return false;
     }
     
@@ -47,7 +58,7 @@ public class Solution {
     }
     public static void main(String[] args) {
     	Solution solution = new Solution();
-    	int[] nums = {4, 3, 2, 3, 5, 2, 1};
-    	System.out.println(solution.canPartitionKSubsets(nums, 4)); //true
+    	System.out.println(solution.canPartitionKSubsets(new int[]{4,3,2,3,5,2,1}, 4)); //true
+    	System.out.println(solution.canPartitionKSubsets(new int[]{5,2,5,5,5,5,5,5,5,5,5,5,5,5,5,3}, 15)); //true
     }
 }
