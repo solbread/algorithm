4c7f835b7babb1a7110f17dc34fc8348cad5f636
JungSol <solbread0@gmail.com>
2017-11-05 15:26:31 +0900

    답안1(try1) solve

---
+++ src/com/algorithm/training/number_theory/eratosthenes/Solution.java
@@ -0,0 +1,32 @@
+package com.algorithm.training.number_theory.eratosthenes;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+	public List<Integer> eratosthenes(int n) {
+		List<Integer> primeList = new ArrayList<>();
+		boolean isPrime[] = new boolean[n];
+		Arrays.fill(isPrime, true);
+		isPrime[0] = isPrime[1] = false;
+		for(int i = 2; i < isPrime.length; i++) {
+			if(isPrime[i]) {
+				for(int j = i*2; j < isPrime.length; j+=i) {
+					isPrime[j] = false;
+				}
+			}
+		}
+		for(int i = 0; i < isPrime.length; i++) {
+			if(isPrime[i]) primeList.add(i);
+		}
+		return primeList;
+	}
+    public static void main(String[] args) {
+		Solution solution = new Solution();
+		List<Integer> primeList = solution.eratosthenes(10);
+		for(int prime : primeList) {
+			System.out.print(prime + " ");
+		}
+	}
+}
