2b0f4d97962049b7ce16d80079ef764bfecc26a0
JungSol <solbread0@gmail.com>
2017-11-08 09:18:27 +0900

    답안1(try2) solve
    (단순실수) trie구조를 만들때 currentTrie를 통해서 포인트를 계속 옮겨줘야 하는데 이부분을 누락하였다.

--- src/com/algorithm/training/trie/longest_word_in_dictionary/Solution.java
+++ src/com/algorithm/training/trie/longest_word_in_dictionary/Solution.java
@@ -9,16 +9,18 @@ public class Solution {
         String longestWord = null;
         for(String word : words) {
             char[] c = word.toCharArray();
+            Object[] currentTrie = trie;
             for(int i = 0; i < word.length(); i++) {
-                if(i < word.length()-1 && trie[c[i]-'a'] == null) {
+                if(i < word.length()-1 && currentTrie[c[i]-'a'] == null) {
                     break;
                 } else if(i == word.length()-1) {
-                    trie[c[i]-'a'] = new Object[26];
+                    currentTrie[c[i]-'a'] = new Object[26];
                     if(longestWord == null || longestWord.length() < word.length() 
                             || (longestWord.length() == word.length() && longestWord.compareTo(word) > 0)) {
                         longestWord = word; 
                     }
                 }
+                currentTrie = (Object[])currentTrie[c[i]-'a'];
             }
         }
         return longestWord;
@@ -29,5 +31,6 @@ public class Solution {
 		System.out.println(solution.longestWord(new String[]{"a", "banana", "app", "appl", "ap", "apply", "apple"})); //apple
 		System.out.println(solution.longestWord(new String[]{"a", "b"})); //a
 		System.out.println(solution.longestWord(new String[]{"abb", "b", "a", "abbc", "ab"})); //abbc
+		System.out.println(solution.longestWord(new String[]{"r","kt","jtgt","j","jtg","rdwy","chkext","c","l","zo","lnp","k","jt","chke","ktui","rd","jtgtha","ch","chkex"})); //jtgt
 	}
 }
