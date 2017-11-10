8a1109f670da452148ed7c58a4cd8f556edc17b5
JungSol <solbread0@gmail.com>
2017-11-10 13:29:40 +0900

    답안1(try1) solve

--- src/com/algorithm/training/dynamic_programming/sushi/BestMain.java
+++ src/com/algorithm/training/dynamic_programming/sushi/BestMain.java
@@ -5,9 +5,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class BestMain {
-	static int[][] shusi;
 	public static void main(String[] args) {
-		BestMain main = new BestMain();
 		Scanner scanner = new Scanner(System.in);
 		try {
 			scanner = new Scanner(new File("src/com/algorithm/training/dynamic_programming/sushi/test"));
@@ -16,14 +14,23 @@ public class BestMain {
 		}
 		int cases = scanner.nextInt();
 		while(cases-- > 0) {
-			shusi = new int[scanner.nextInt()][2];
-			int budget = scanner.nextInt();
+			int shusi[][] = new int[scanner.nextInt()][2];
+			int dp[] = new int[201];
+			int budget = scanner.nextInt() / 100;
 			for(int i = 0; i < shusi.length; i++) {
 				for(int j = 0; j < shusi[i].length; j++) {
 					shusi[i][j] = scanner.nextInt();
+					if(j==0) shusi[i][j] /= 100;
 				}
 			}
-			
+			for(int i = 1; i <= budget; i++) {
+			    for(int j = 0; j < shusi.length; j++) {
+			        if(i >= shusi[j][0]) {
+			            dp[i%201] = Math.max(dp[i%201], dp[(i-shusi[j][0])%201] + shusi[j][1]);
+			        }
+			    }
+			}
+			System.out.println(dp[budget%201]);
 		}
 	}
 }
