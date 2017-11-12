35ec777b242ceedab057f6455884e9d7a9f35f62
JungSol <solbread0@gmail.com>
2017-11-12 23:45:16 +0900

    답안2(try2) TLE
    1. 블록을 놓을 수 있는 4가지 방법의 좌표를 계산하여 저장해두는 settingCase에 중복된 블록의 형태는 들어가지 않도록 개선
    2. BufferedReader를 이용한 빠른 입출력 적용

--- src/com/algorithm/training/combinatorial_search/boardcover2/BestMain.java
+++ src/com/algorithm/training/combinatorial_search/boardcover2/BestMain.java
@@ -1,28 +1,36 @@
 package com.algorithm.training.combinatorial_search.boardcover2;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class BestMain {
 	static char[][] board, block;
-	static int[][][] settingCase;
+	static List<List<int[]>> settingCase;
 	static int blockWidth, blockHeight, maxBlockCount;
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		BestMain main = new BestMain();
-		Scanner scanner = new Scanner(System.in);
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 		try {
-			scanner = new Scanner(new File("src/com/algorithm/training/combinatorial_search/boardcover2/test"));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(new File("src/com/algorithm/training/combinatorial_search/boardcover2/test"))));
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
 		}
-		int cases = scanner.nextInt();
+		int cases = Integer.parseInt(bufferedReader.readLine());
 		while(cases-- > 0) {
 			maxBlockCount = 0;
-			board  = new char[scanner.nextInt()][scanner.nextInt()];
-			int blockWidth = scanner.nextInt(), blockHeight = scanner.nextInt(), blanckCount = 0;
+			StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
+			board  = new char[Integer.parseInt(stringTokenizer.nextToken())][Integer.parseInt(stringTokenizer.nextToken())];
+			int blockWidth = Integer.parseInt(stringTokenizer.nextToken()), blockHeight = Integer.parseInt(stringTokenizer.nextToken()), blanckCount = 0;
 			for(int i = 0;  i < board.length; i++) {
-				board[i] = scanner.next().toCharArray();
+				board[i] = bufferedReader.readLine().toCharArray();
 				for(int j = 0; j < board[i].length; j++) {
 					if(board[i][j] == '.') blanckCount++;
 				}
@@ -30,7 +38,7 @@ public class BestMain {
 			int blockNumber = 0;
 			block = new char[blockWidth][blockHeight];
 			for(int i = 0; i < block.length; i++) {
-				block[i] = scanner.next().toCharArray();
+				block[i] = bufferedReader.readLine().toCharArray();
 				for(int j = 0; j < block[i].length; j++) {
 					if(block[i][j] == '#') blockNumber++;
 				}
@@ -42,9 +50,9 @@ public class BestMain {
 	}
 	
 	public void makeSettingCase(int blockNumber) {
-		settingCase = new int[4][blockNumber][2];
-		for(int i = 0; i < settingCase.length; i++) {
-			int index = 0;
+		settingCase = new ArrayList<>();
+		for(int i = 0; i < 4; i++) {
+			List<int[]> setting = new ArrayList<>();
 			int leftTop[] = {-1,-1};
 			for(int j = 0; j < block.length; j++) {
 				for(int k = 0; k < block[j].length; k++) {
@@ -53,11 +61,19 @@ public class BestMain {
 							leftTop[0] = j;
 							leftTop[1] = k;
 						}
-						settingCase[i][index] = new int[]{j-leftTop[0], k-leftTop[1]};
-						index++;
+						setting.add(new int[]{j-leftTop[0], k-leftTop[1]});
 					}
 				}
 			}
+			Collections.sort(setting, (a,b) -> (a[0]==b[0] ? a[1]-b[1] : a[0]-b[0]));
+			boolean isSame = true;
+			for(List<int[]> addedSetting : settingCase) {
+				for(int j = 0; j < setting.size(); j++) {
+					isSame = isSame && addedSetting.get(j)[0] == setting.get(j)[0]
+							&& addedSetting.get(j)[1] == addedSetting.get(j)[1];
+				}
+			}
+			if(settingCase.isEmpty() || !isSame) settingCase.add(setting);
 			rotateBlock();
 		}
 	}
@@ -74,16 +90,16 @@ public class BestMain {
 	
 	
 	public void countMaxBlock(int currentBlockCount, int remainedBlanckCount) {
-		if(currentBlockCount + remainedBlanckCount / settingCase[0].length <= maxBlockCount) return;
+		if(currentBlockCount + remainedBlanckCount / settingCase.get(0).size() <= maxBlockCount) return;
 		boolean cover = false;
 		for(int i = 0; i < board.length && !cover; i++) {
 			for(int j = 0; j < board[i].length && !cover; j++) {
 				if(board[i][j] == '.') {
 					cover = true;
-					for(int[][] settings : settingCase) {
+					for(List<int[]> settings : settingCase) {
 						if(canSet(settings, i, j)) {
 							setBlock(settings, i, j, '#');
-							countMaxBlock(currentBlockCount+1, remainedBlanckCount-settingCase[0].length);
+							countMaxBlock(currentBlockCount+1, remainedBlanckCount-settingCase.get(0).size());
 							setBlock(settings, i, j, '.');
 						}
 					}
@@ -96,7 +112,7 @@ public class BestMain {
 		if(!cover) maxBlockCount = Math.max(maxBlockCount, currentBlockCount);
 	}
 
-	private boolean canSet(int[][] settings, int cX, int cY) {
+	private boolean canSet(List<int[]> settings, int cX, int cY) {
 		boolean canSet = true;
 		for(int[] setting : settings) {
 			int x = setting[0];
@@ -108,7 +124,7 @@ public class BestMain {
 		return canSet;
 	}
 	
-	private void setBlock(int[][] settings, int cX, int cY, char setChar) {
+	private void setBlock(List<int[]> settings, int cX, int cY, char setChar) {
 		for(int[] set : settings) {
 			board[cX+set[0]][cY+set[1]] = setChar;
 		}
