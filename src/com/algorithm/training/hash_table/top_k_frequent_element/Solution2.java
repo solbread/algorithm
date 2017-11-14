063940e2cfcf77b4338685ed851b5f2072180051
JungSol <solbread0@gmail.com>
2017-11-14 21:24:26 +0900

    답안2(try2) solve
    빈도수배열의 max값은 n이므로 frequenceCounter[n]을 했을떄 에러가 나지 않도록 배열의 크기 변경

--- src/com/algorithm/training/hash_table/top_k_frequent_element/Solution2.java
+++ src/com/algorithm/training/hash_table/top_k_frequent_element/Solution2.java
@@ -38,5 +38,10 @@ public class Solution2 {
         for( int num : result) {
             System.out.print(num + " ");
         }
+        System.out.println();
+        result = solution.topKFrequent(new int[]{1}, 1);
+        for( int num : result) {
+            System.out.print(num + " ");
+        }
     }
 }
