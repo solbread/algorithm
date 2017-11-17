018ce809088b549a5643527af1c5347bb35ca64f
JungSol <solbread0@gmail.com>
2017-11-17 14:08:57 +0900

    답안1(try2) wrong answer
    two pointer를 가지고 물이 고이는 지점을 찾았을 때 넓이를 개산하는 부분을 left를 기준으로 다 넓이를 구했다가 left보다 right가 적어서 left가 기준이아니라 right가 기준이 되어야할때 height[right]-height[left] * x축길이를 넓이에서 빼주는 방식으로 했는데, 이렇게 하면 {5,4,1,2}와 같은예제에서 left=5, right=2이므로 더 적은
     2가 기준높이인데 height[1]인 4는 2보다 크므로 넓이가 0이어야 하는데 마이너스가 되어버리므로 잘못계산이 되었다. 그래서 stack을 left랑 right를 찾으면 스택에서 하나씩 pop하면서 넓이를 계산하도록 수정하였다.

--- src/com/algorithm/training/greedy/trapping_rain_water/Solution.java
+++ src/com/algorithm/training/greedy/trapping_rain_water/Solution.java
@@ -1,28 +1,32 @@
 package com.algorithm.training.greedy.trapping_rain_water;
 
+import java.util.Stack;
+
 public class Solution {
     public int trap(int[] height) {
+        Stack<Integer> stack = new Stack<>();
         boolean isDown = true;
         int i = 0;
         while(i < height.length && height[i] == 0) {
             i++;
         }
-        int left = i, trapSize = 0, currentTrapSize = 0;
+        int left = i, trapSize = 0;
         for(i+=1; i < height.length; i++) {
             if(isDown && height[i-1] < height[i]) {
                 isDown = false;
             } 
             if(!isDown && (i == height.length-1 || height[i] > height[i+1])) {
-                if(height[left] > height[i]) {
-                    currentTrapSize -= (height[left] - height[i]) * (i - left - 1);
+                int stdHeight = Math.min(height[left], height[i]);
+                while(!stack.isEmpty()) {
+                    int curHeight = stack.pop();
+                    if(curHeight < stdHeight) {
+                        trapSize += (stdHeight - curHeight);
+                    }
                 }
-                trapSize += currentTrapSize;
-                currentTrapSize = 0;
                 left = i;
                 isDown = true;
-            }
-            if(height[left] > height[i]) {
-                currentTrapSize += (height[left] - height[i]);
+            } else {
+                stack.add(height[i]);
             }
         }
         return trapSize;
@@ -32,5 +36,6 @@ public class Solution {
         Solution solution = new Solution();
         System.out.println(solution.trap(new int[]{}));
         System.out.println(solution.trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));
+        System.out.println(solution.trap(new int[]{5,4,1,2}));
     }
 }
