19c462ff9417592e9670aec91222e54e246beddd
JungSol <solbread0@gmail.com>
2017-11-20 08:33:24 +0900

    답안1(try1) solve

--- src/com/algorithm/training/two_pointers/trapping_rain_water/Solution.java
+++ src/com/algorithm/training/two_pointers/trapping_rain_water/Solution.java
@@ -37,5 +37,6 @@ public class Solution {
         System.out.println(solution.trap(new int[]{}));
         System.out.println(solution.trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));
         System.out.println(solution.trap(new int[]{5,4,1,2}));
+        System.out.println(solution.trap(new int[]{5,2,1,2,1,5}));
     }
 }
