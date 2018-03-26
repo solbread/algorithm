a1873cb31eeb337ce63435f7eb924373c0e47419
JungSol <solbread0@gmail.com>
2018-03-26 19:32:13 +0900

    try2 (solve)
    A문자열을 shift연산해서 B문자열로 만들어야 하므로 A문자열과 B문자열의 길이는 같아야한다.

--- src/com/algorithm/training/string/rotate_string/Solution.java
+++ src/com/algorithm/training/string/rotate_string/Solution.java
@@ -2,12 +2,13 @@ package com.algorithm.training.string.rotate_string;
 
 public class Solution {
     public boolean rotateString(String A, String B) {
-        return (A+A).contains(B);
+        return A.length() == B.length() && (A+A).contains(B);
     }
     
     public static void main(String[] args) {
 		Solution solution = new Solution();
 		System.out.println(solution.rotateString("abcde", "cdeab")); //true
 		System.out.println(solution.rotateString("abcde", "abced")); //false
+		System.out.println(solution.rotateString("aa", "a")); //false
 	}
 }
