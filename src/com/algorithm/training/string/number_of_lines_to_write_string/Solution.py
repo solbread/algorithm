e95eba16d103534ea5e84ac2b975ff4efb26a47c
JungSol <solbread0@gmail.com>
2018-04-06 00:10:56 +0900

    try2 solve
    각 문자는 각 문자의 길이만큼이 비어있어야지 line에 write될 수 있다는 것을 몰랐었어서, 이부분을 적용하여 코드 수정

--- src/com/algorithm/training/string/number_of_lines_to_write_string/Solution.py
+++ src/com/algorithm/training/string/number_of_lines_to_write_string/Solution.py
@@ -8,13 +8,18 @@ class Solution:
         :type S: str
         :rtype: List[int]
         """
-        _total_length = 0
+        _line_number = 1
+        _last_line_width = 0
         for c in S:
-            _total_length += widths[ord(c)-ord('a')]
-        return [math.ceil(_total_length / 100), _total_length % 100]
+            _current_width = widths[ord(c)-ord('a')]
+            _line_number += 1 if _last_line_width + _current_width > 100 else 0
+            _last_line_width = _current_width if _last_line_width + _current_width > 100 else _last_line_width + _current_width
+        return [_line_number, _last_line_width]
 
 
 if __name__ == "__main__":
     solution = Solution()
     widths = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
-    print(solution.numberOfLines(widths, 'abcdefghijklmnopqrstuvwxyz'))
+    print(solution.numberOfLines(widths, 'abcdefghijklmnopqrstuvwxyz')) #3, 60
+    widths = [4, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
+    print(solution.numberOfLines(widths, 'bbbcccdddaaa')) #2, 4
