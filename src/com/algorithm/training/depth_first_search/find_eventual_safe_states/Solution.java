964b2ef281317c12d1ff7690dc604b4622ac53e2
JungSol <solbread0@gmail.com>
2018-04-07 03:38:52 +0900

    try3 sovle
    memorization이용하여 tle 해결

--- src/com/algorithm/training/depth_first_search/find_eventual_safe_states/Solution.java
+++ src/com/algorithm/training/depth_first_search/find_eventual_safe_states/Solution.java
@@ -7,21 +7,22 @@ public class Solution {
     int[][] graph;
     public List<Integer> eventualSafeNodes(int[][] graph) {
         this.graph = graph;
+        int[] visitied = new int[graph.length];
         List<Integer> safeNodes = new ArrayList<>();
         for(int i = 0; i < graph.length; i++) {
-            if(isSafe(i, new boolean[graph.length])) safeNodes.add(i);
+            if(isSafe(i, visitied)) safeNodes.add(i);
         }
         return safeNodes;
     }
     
-    private boolean isSafe(int visitNodeNumber, boolean[] visited) {
-        if(visited[visitNodeNumber]) return false;
+    private boolean isSafe(int visitNodeNumber, int[] visited) {
+        if(visited[visitNodeNumber] != 0) return visited[visitNodeNumber] == 1;
         boolean isSafe = true;
-        visited[visitNodeNumber] = true;
+        visited[visitNodeNumber] = 2;
         for(int i = 0; isSafe && i < graph[visitNodeNumber].length; i++) {
              isSafe = isSafe(graph[visitNodeNumber][i], visited);
         }
-        visited[visitNodeNumber] = false;
+        visited[visitNodeNumber] = isSafe ? 1 : 2;
         return isSafe;
     }
     
