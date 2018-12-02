102bfa13b822528ae6182fc250f28b605cef816f
solbread <solbread.dev@gmail.com>
2018-12-03 03:15:16 +0900

    solve problem (try1)

--- src/com/algorithm/contest/advent_of_code_2018/day02/puzzle2/Solution.java
+++ src/com/algorithm/contest/advent_of_code_2018/day02/puzzle2/Solution.java
@@ -1,35 +1,40 @@
-package com.algorithm.contest.advent_of_code_2018.day02.puzzle1;
+package com.algorithm.contest.advent_of_code_2018.day02.puzzle2;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Solution {
     public static void main(String[] args) {
         try {
-            FileReader fileReader = new FileReader("src/com/algorithm/contest/advent_of_code_2018/day02/puzzle1/input");
+            FileReader fileReader = new FileReader("src/com/algorithm/contest/advent_of_code_2018/day02/puzzle2/input");
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String line = null;
-            int twoLetterCnt = 0, threeLetterCnt = 0;
+            List<String> input = new ArrayList<>();
             while((line = bufferedReader.readLine())!= null) {
-                Map<Character, Integer> charCount = new HashMap<>();
-                for (int i = 0; i < line.length(); i++) {
-                    charCount.put(line.charAt(i), charCount.getOrDefault(line.charAt(i),0) + 1);
-                }
-                boolean existTwoLetter = false, existThreeLetter= false;
-                for(int count : charCount.values()) {
-                    if(!existTwoLetter && count == 2) {
-                        twoLetterCnt++;
-                        existTwoLetter = true;
-                    } else if (!existThreeLetter && count == 3) {
-                        threeLetterCnt++;
-                        existThreeLetter = true;
+                input.add(line);
+            }
+            boolean found = false;
+            for(int i = 0; !found && i < input.size(); i++) {
+                for(int j = i+1; !found && j < input.size(); j++) {
+                    int diff = 0, diffIdx = 0;
+                    for(int k = 0; k < input.get(i).length(); k++) {
+                        if(input.get(i).charAt(k) != input.get(j).charAt(k)) {
+                            diff++;
+                            diffIdx = k;
+                        }
+                        if(diff > 2) break;
+                    }
+                    if(diff == 1) {
+                        System.out.print(input.get(i).substring(0, diffIdx) + input.get(i).substring(diffIdx+1, input.get(i).length()));
+                        found = true;
                     }
                 }
             }
-            System.out.print(twoLetterCnt * threeLetterCnt);
         } catch (IOException e) {
             e.printStackTrace();
         }
