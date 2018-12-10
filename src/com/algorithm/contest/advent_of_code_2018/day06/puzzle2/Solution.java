56aa865aa82d171cde818c47c2858df58fa2e2d3
JungSol <solbread0@gmail.com>
2018-12-11 01:28:28 +0900

    merge

--- src/com/algorithm/contest/advent_of_code_2018/day06/puzzle2/Solution.java
+++ src/com/algorithm/contest/advent_of_code_2018/day06/puzzle2/Solution.java
@@ -48,6 +48,30 @@ public class Solution {
                 }
             }
             int regionCount = 0;
+        try(FileReader fileReader = new FileReader("src/com/algorithm/contest/advent_of_code_2018/day06/puzzle1/input");
+                BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            String line;
+            List<int[]> points = new ArrayList<int[]>();
+            int maxX = 0, maxY = 0;
+            while((line = bufferedReader.readLine()) != null) {
+                int y = Integer.parseInt(line.split(", ")[0]);
+                int x = Integer.parseInt(line.split(", ")[1]);
+                points.add(new int[2]);
+                points.get(points.size()-1)[0] = x;
+                points.get(points.size()-1)[1] = y;
+                maxX = Math.max(maxX, x);
+                maxY = Math.max(maxY, y);
+            }
+            int grid[][] = new int[maxX+1][maxY+1];
+            for(int al = 0; al < points.size(); al++) {
+                for(int i = 0; i < grid.length; i++) {
+                    for(int j = 0; j < grid[i].length; j++) {
+                        int curDistance = getDistance(points.get(al), new int[] {i,j});
+                        grid[i][j] += curDistance;
+                    }
+                }
+            }
+            int regionCount = 0;
             for(int i = 0; i < grid.length; i++) {
                 for(int j = 0; j < grid[i].length; j++) {
                     if(grid[i][j] < 10000) {
